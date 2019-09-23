da83ba8754cb48dd43abcbbc9a8167fec2c1ffff
Russ Brooks <me@russbrooks.com>
2019-09-22 22:13:15 -0400

    Add SCSS / SASS support

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,47 @@
 const path = require('path');
 
 module.exports = {
-    entry: { main: './wwwroot/src/components/app.js' },
-    mode: 'development',
+    entry: { main: ['./wwwroot/src/components/app.js', './wwwroot/src/scss/app.scss'] },
     output: {
         path: path.resolve(__dirname, 'wwwroot/dist'),
-        filename: 'bundle.js',
         publicPath: '/dist/'
     },
     module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-react', '@babel/preset-env']
-                    }
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-react', '@babel/preset-env']
                 }
             }
-        ]
+        }, {
+            test: /\.(css|scss)$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'css/[name].css',
+                    }
+                },
+                {
+                    loader: 'extract-loader'
+                },
+                {
+                    loader: 'css-loader?-url',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }
+            ]
+        }]
     }
 };
+
